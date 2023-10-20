8bd5e1e05723a6535557fb89a5f50ffcb6c705c6
RainMark <RainMark@users.noreply.github.com>
2023-10-20 15:01:08 +0000

    myrocks

--- example/os_start.sh
+++ example/os_start.sh
@@ -1,4 +1,2 @@
-sudo docker run -dt --name os_mariadb -v $PWD/DBdata:/var/lib/mysql -v $PWD/Files:/Files linuxstory/mariadb:0.0.2 entrypoint.sh
-sudo docker run -dt --name os_wp_php --tmpfs /fastcgi-cache --tmpfs /tmp --link os_mariadb --sysctl net.core.somaxconn=80000 -v $PWD/os:/var/www -p 6789:80 linuxstory/wordpress:php7.4 entrypoint.sh --server 127.0.0.1 --site-root /var/www
-
-
+sudo docker run -dt --name myrocks -v $PWD/DBdata:/var/lib/mysql -v $PWD/Files:/Files linuxstory/mariadb:myrocks entrypoint.sh --init
+sudo docker run -dt --name os_wp_php_myrocks --tmpfs /tmp --link myrocks --sysctl net.core.somaxconn=80000 -v $PWD/os:/var/www -p 6789:80 linuxstory/wordpress:php7.4 entrypoint.sh --server 127.0.0.1 --site-root /var/www
--- mariadb/entrypoint.sh
+++ mariadb/entrypoint.sh
@@ -6,7 +6,8 @@ sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 if [ "--init" = "$1" ]; then
 	mysql_install_db
+	chown -R mysql:mysql /var/lib/mysql
 fi
 
-service mysql start
+service mariadb start
 exec bash
